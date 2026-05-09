5120503dc07d25e963a4508f42b319c7e574286e
Aneesa-Ran <itsaneesa123@gmail.com>
2026-05-09 02:37:56 -0700

    Initial Commit

---
+++ main.dart
@@ -0,0 +1,96 @@
+import 'dart:io';
+
+void main() {
+  /*
+  print("Enter your marks:");
+
+  int marks = int.parse(stdin.readLineSync()!);
+
+  if (marks < 50) {
+    print("You are Failed");
+  } 
+  else if (marks < 70) {
+    print("Grade: C");
+  }
+   else if (marks < 80) {
+    print("Grade: B");
+  }
+   else if (marks < 90) {
+    print("Grade: A");
+  } 
+  else {
+    print("Grade: A+");
+  } 
+
+ //Program for calculating Electricity Bills
+
+ stdout.write('Enter consumed Unit:');
+ int consumedUnit = int.parse(stdin.readLineSync()!);
+ int finalPrice = 0;
+
+ if(consumedUnit <100){
+    finalPrice =consumedUnit * 20;
+    print('Your Electricity Bill is $finalPrice');
+    print('Consumed Units$consumedUnit');
+    print('Per Unit is 20');
+}
+   else if(consumedUnit >100 && consumedUnit <200 ){
+      finalPrice =consumedUnit * 30;
+    print('Your Electricity Bill is $finalPrice');
+    print('Consumed Units$consumedUnit');
+    print('Per Unit is 30');
+   }
+   else if(consumedUnit > 200 && consumedUnit < 300){
+      finalPrice =consumedUnit * 40;
+    print('Your Electricity Bill is $finalPrice');
+    print('Consumed Units$consumedUnit');
+    print('Per Unit is 40');
+   }
+   else{
+    finalPrice =consumedUnit * 50;
+    print('Your Electricity Bill is $finalPrice');
+    print('Consumed Units$consumedUnit');
+    print('Per Unit is 50');
+   }
+  
+
+  // Dart For Loop 
+ 
+  for( int x = 0; x<=10; x++){
+    print(x*x+1/2);
+
+  }
+   
+  for (int i = 0; i<=20 ; i++){
+    if(i % 2 ==0){
+      print('$i is an even number');
+    }
+    else{
+      print('$i is a odd number');
+    }
+  }
+  
+  
+ stdout.write('Enter a Number');
+ int x = int.parse(stdin.readLineSync()!);
+ for(int i = 1; i <=20; i++){
+  print('$x x i =${i * x}');
+ }
+ Dart list 
+
+   List<int> age = [];
+   int newAge = 0;
+   stdout.write("Please Enter Number:");
+   int inputCount = int.parse(stdin.readLineSync()!);
+
+   for (int i = 0; i< inputCount ; i++){
+    print('Enter Age');
+    newAge = int.parse(stdin.readLineSync()!) ;
+    age.add(newAge);
+   }
+   
+   for(int i = 0; i< age.length; i++){
+    print('age ${age[i]}');
+   }*/ 
+ 
+}
