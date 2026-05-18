b1fe41050ee91619ecd49f20bf4e0a5e9522fe9f
Aneesa-Rani <146238026+Aneesa-Rani@users.noreply.github.com>
2026-05-18 07:53:22 -0700

    Initial Commit

--- main.dart
+++ main.dart
@@ -8,6 +8,15 @@ void main(){
  
   Person person = Person();
   person.displayInfo();
+   
+  Student student = Student();
+  student.name = 'Balaj Khan';
+  student.age = 8;
+  student.semester = ' Autumn 2025';
+  student.studentId = 0000574350;
+  student.grade = 'A++';
+  student.upgradeGrade('B');
+  student.displayInfo();
 }
  
 class Person{
@@ -19,7 +28,36 @@ class Person{
   void displayInfo(){
     print('My name is ${name}');
        print('My Fathername is ${fatherName}');
-          print('My age is ${name}');
+          print('My age is ${age}');
+          print('..........................');
   }
+
+
+
+}
+
+// Pratice of classes and objects
+class Student{
+  String? name;
+  int? age ;
+  String? semester;
+  int? studentId ;
+  String? grade;
+
+ void displayInfo(){
+  print('Student name is : $name');
+   print('Student age is : $age');
+    print('Student semester is : $semester');
+     print('Student Id is : $studentId');
+     print('Student Grade is : $grade');
+     print('..........................');
+ }
+
+ 
+       void upgradeGrade(String newGrade){
+        grade = newGrade;
+        print("Upgrade Grade of the student is :$newGrade for $name");
+
+ }
 
 }
